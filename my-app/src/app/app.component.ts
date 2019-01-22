c2b00ebdaa76997378d79896636fcf59e3cd0cc2
CasperStuyts <33087689+CasperStuyts@users.noreply.github.com>
2019-01-22 14:05:07 +0100

    interface vraag 1
    interface vraag 1 gemaakt

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Examen WebTech 3 Vraag 1';
+}
